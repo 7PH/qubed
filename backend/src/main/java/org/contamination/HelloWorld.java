2a7a7217e075005575e0423c36c7eb049f9f3729
Wojtek Wajerowicz <115081248+wojciech-wajerowicz-sonarsource@users.noreply.github.com>
2023-09-21 11:39:02 +0200

    Use javax websocket.

--- backend/src/main/java/org/contamination/HelloWorld.java
+++ backend/src/main/java/org/contamination/HelloWorld.java
@@ -1,19 +1,32 @@
 package org.contamination;
+
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.websocket.server.WebSocketHandler;
-import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.websocket.javax.server.config.JavaxWebSocketServletContainerInitializer;
 
 public class HelloWorld {
-
   public static void main(String[] args) throws Exception {
+
     Server server = new Server(8080);
-    WebSocketHandler wsHandler = new WebSocketHandler() {
-      @Override
-      public void configure(WebSocketServletFactory factory) {
-        factory.register(Websocket.class);
-      }
-    };
-    server.setHandler(wsHandler);
+    ServerConnector connector = new ServerConnector(server);
+    server.addConnector(connector);
+    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+    context.setContextPath("/");
+    server.setHandler(context);
+
+    JavaxWebSocketServletContainerInitializer.configure(context, (servletContext, wsContainer) ->
+    {
+      // This lambda will be called at the appropriate place in the
+      // ServletContext initialization phase where you can initialize
+      // and configure  your websocket container.
+
+      // Configure defaults for container
+      wsContainer.setDefaultMaxTextMessageBufferSize(65535);
+
+      // Add WebSocket endpoint to javax.websocket layer
+      wsContainer.addEndpoint(Endpoint.class);
+    });
     server.start();
     server.join();
   }
