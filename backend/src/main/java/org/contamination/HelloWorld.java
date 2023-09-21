b44bf3ce7fa695230549edc3727a7ba06ff5c3dd
lukasz-jarocki-sonarsource <lukasz.jarocki@sonarsource.com>
2023-09-21 11:27:35 +0200

    Added websocket connection on 8080

--- backend/src/main/java/org/contamination/HelloWorld.java
+++ backend/src/main/java/org/contamination/HelloWorld.java
@@ -1,7 +1,20 @@
 package org.contamination;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.websocket.server.WebSocketHandler;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 
 public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello, World!");
+
+  public static void main(String[] args) throws Exception {
+    Server server = new Server(8080);
+    WebSocketHandler wsHandler = new WebSocketHandler() {
+      @Override
+      public void configure(WebSocketServletFactory factory) {
+        factory.register(Websocket.class);
+      }
+    };
+    server.setHandler(wsHandler);
+    server.start();
+    server.join();
   }
 }
