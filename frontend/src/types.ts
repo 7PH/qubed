c95748a1377b2f55851f541285fb95e49e172aa0
Jeremy Davis <jeremy.davis@sonarsource.com>
2023-09-21 15:55:41 +0200

    send ready state

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,13 +1,17 @@
-
-export type Player = {
-    id: number;
-    name: string;
-    ready: boolean;
-    infected: boolean;
-    position: [number, number]
+export enum PlayerStatus {
+  Waiting = "WAITING",
+  Ready = "READY",
 }
 
+export type Player = {
+  id: number;
+  name: string;
+  status: PlayerStatus;
+  infected: boolean;
+  position: [number, number];
+};
+
 export enum GameState {
-    PENDING = 'pending',
-    RUNNING = 'running'
+  PENDING = "pending",
+  RUNNING = "running",
 }
